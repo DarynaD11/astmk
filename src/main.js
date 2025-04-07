7474e76f7578216f61ad4123cc843046263361b5
Daryna <darinadyadyuk11@gmail.com>
2025-04-07 20:49:45 +0300

    add

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-import './js/mob-menu.js';
+async function loadHTML(selector, url) {
+  const el = document.querySelector(selector);
+  if (el) {
+    const res = await fetch(url);
+    const html = await res.text();
+    el.innerHTML = html;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadHTML('#header', './partials/header.html');
+  loadHTML('#hero', './partials/hero.html');
+  loadHTML('#informations', './partials/informations.html');
+  loadHTML('#competitions', './partials/competitions.html');
+  loadHTML('#news', './partials/news.html');
+  loadHTML('#partners', './partials/partners.html');
+  loadHTML('#footer', './partials/footer.html');
+  loadHTML('#mob-menu', './partials/mob-menu.html');
+});
